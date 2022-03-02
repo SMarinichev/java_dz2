42167f8a1fe33aabaecde71e11bec7b1db583e20
SMarinichev <ssmarinichev@ya.ru>
2022-03-02 16:06:16 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 666;
+        int bonusMile = 20;
+        int bonus = ticketPrice / bonusMile;
+
+        System.out.println("Начислено миль");
+        System.out.println(bonus);
+
+    }
+}
